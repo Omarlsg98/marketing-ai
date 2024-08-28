6d1cb017813dad5f5bb153e68d2baaac232d176a
Omar_Sanchez <ols@andrew.cmu.edu>
2024-08-27 22:47:05 -0400

    fix: no more than 1000 tokens error

--- lib/server/persona/prompts.ts
+++ lib/server/persona/prompts.ts
@@ -11,7 +11,7 @@ poised and confident demeanor.
 
 Keep it succinct and to the point, focusing on the most salient details of the subject's most probable appearance.
 Feel free to came up with details that are not present in the context, but that would make sense given the context.
-Keep it short. Don't mention clothes.
+Keep it short just 1 or two sentences. Don't mention clothes.
 Answer only the question asked and don't make any reference to this instructions
 `
 };
@@ -22,19 +22,18 @@ const getApparelPrompt = (context: string) => {
 Giving the previous context of the subject generate a description of the apparel like the following:
 Example: She is wearing a deep blue blouse, which 
 offers a sharp contrast to the white backdrop and complements her light olive 
-skin tone. Accessories, if present, such as a simple silver necklace, 
-lend a touch of elegance to the portrait.
+skin tone. Accessories, such as a simple silver necklace, lend a touch of elegance to the portrait.
 
 Keep it succinct and to the point, focusing on the most salient details of the subject's most probable wardrobe.
 Feel free to came up with details that are not present in the context, but that would make sense given the context.
-Keep it short. Focus only on clothes from the shoulders up.
+Keep it short, just 1 or two sentences. Focus only on clothes from the shoulders up.
 Answer only the question asked and don't make any reference to this instructions
 `
 };
 
 
 const getGenerateImagePrompt = (mainDescription: string, apparel:string) => {
-  return `Create a professional headshot photograph set against a seamless white background.
+  return `Create a professional headshot photograph set against a slightly grey background.
 ${mainDescription}. The lighting is studio quality, providing a 
 flattering illumination to the subject's delicate facial features without 
 casting any distracting shadows. ${apparel}. The composition is tightly framed, 
