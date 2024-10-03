56baeddad0a25b1a03c58240bb534cf85f10e339
ty-interseed <tyrone@interseed.ai>
2024-10-03 11:04:24 +0100

    update message bubble and sidenav controls

--- config.ts
+++ config.ts
@@ -5,8 +5,7 @@ const config = {
   // REQUIRED
   appName: "Interseed AI",
   // REQUIRED: a short description of your app for SEO tags (can be overwritten)
-  appDescription:
-    "Simplified Marketing",
+  appDescription: "Simplified Marketing",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
   domainName: "interseed.ai",
   crisp: {
@@ -231,9 +230,9 @@ const config = {
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
     theme: "light",
-    // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
+    // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme.
     // OR you can just do this to use a custom color: main: "#f37055". HEX only.
-    main: themes[`[data-theme=light]`]["primary"],
+    main: themes["light"]["primary"],
   },
   auth: {
     // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
